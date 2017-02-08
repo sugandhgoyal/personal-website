fb779a7d0fdf6518d588e7413dc154c274d263bd
sugandh <goyalsugandh2@gmail.com>
2017-02-08 20:12:10 +0530

    css files

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.
+    width = "700px";
+    document.getElementById("mySidenav").style.
+    height = "700px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.
+    width = "0";
+     document.getElementById("mySidenav").style.
+    height = "0";
+}
+
